44d4dda4c0a6ff68831d12da9d036237bead67ee
Shlomi Shitrit <akunh113@gmail.com>
2023-11-18 19:25:50 +0200

    add repositories and and interfaces

---
+++ Backend/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+using Backend.Models;
+
+namespace Backend.Interfaces
+{
+    public interface IUserRepository
+    {
+        public ICollection<User>? GetUsers();
+        public User? GetUserById(int id);
+        public bool UserExists(int id);
+        public bool CreateUser(User user);
+        public bool UpdateUser(User user);
+        public bool DeleteUser(User user);
+        public ICollection<Category> GetCategories(int userId);
+        public bool Save();
+    }
+}
