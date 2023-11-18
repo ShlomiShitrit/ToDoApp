f49feb50ef55893005faf152067f04690a01ade9
Shlomi Shitrit <akunh113@gmail.com>
2023-11-18 19:27:35 +0200

    update program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,5 +1,19 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Backend.Interfaces;
+using Backend.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ISubCategoryRepository, SubCategoryRepository>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
