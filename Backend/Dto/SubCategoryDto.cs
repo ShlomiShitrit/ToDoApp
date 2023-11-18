74208d69ace31b7ecd2fac723d4b479f0e400abf
Shlomi Shitrit <akunh113@gmail.com>
2023-11-18 19:26:07 +0200

    add dto

---
+++ Backend/Dto/SubCategoryDto.cs
@@ -0,0 +1,8 @@
+namespace Backend.Dto
+{
+    public class SubCategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}   
---
+++ Backend/Dto/TaskDto.cs
@@ -0,0 +1,9 @@
+namespace Backend.Dto
+{
+    public class TaskDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+    }
+}
