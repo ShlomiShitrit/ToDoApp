df0c6643487fc076a3577fad78ff8e168be1fb8d
Shlomi Shitrit <akunh113@gmail.com>
2023-11-29 18:20:13 +0200

    Build home screen Fixes #6

--- Frontend/Components/Screens/HomeScreen.tsx
+++ Frontend/Components/Screens/HomeScreen.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import {Text} from '@rneui/themed';
+import React, {useState, useEffect} from 'react';
+import {Text, ListItem} from '@rneui/themed';
 import {useAppSelector} from '../../hooks/store';
-import {View, StyleSheet} from 'react-native';
+import {EMPTY_WEEKLY_TASKS} from '../../general/resources';
+import {ITask} from '../../general/interfaces';
+import {View, ScrollView, StyleSheet, Dimensions} from 'react-native';
+
+const windowHeight = Dimensions.get('window').height;
 
 export default function HomeScreen(): JSX.Element {
+  const [weeklyTasks, setWeeklyTasks] = useState<ITask[]>(EMPTY_WEEKLY_TASKS);
+
   const userFirstName = useAppSelector(state => state.user.userInfo.firstName);
   const userLastName = useAppSelector(state => state.user.userInfo.lastName);
-  const tasksNumber = 3; // TODO: get tasks number from store
 
+  useEffect(() => {
+    get
+  }, []);
+
+  const date = new Date();
   return (
     <View style={styles.container}>
       <Text
@@ -15,9 +25,41 @@ export default function HomeScreen(): JSX.Element {
         h1Style={
           styles.title
         }>{`Welcome ${userFirstName} ${userLastName}`}</Text>
-      <Text h4 h4Style={styles.text}>
-        {`You have ${tasksNumber} tasks to complete`}
+      <Text h4 h4Style={styles.date}>
+        {date.toLocaleDateString()}
       </Text>
+      <View style={styles.taskContainer}>
+        {weeklyTasks && Array.isArray(weeklyTasks) && weeklyTasks.length > 0 ? (
+          <ScrollView style={styles.scrollViewContainer}>
+            {weeklyTasks.map((task, index) => (
+              <ListItem key={index} bottomDivider>
+                <ListItem.Content>
+                  <ListItem.Title style={styles.itemTitle}>
+                    {task?.title}
+                  </ListItem.Title>
+                  <ListItem.Subtitle style={styles.itemSubTitle}>
+                    {task?.subTitle}
+                  </ListItem.Subtitle>
+                  <ListItem.Subtitle style={styles.itemDate}>
+                    {date.toLocaleTimeString()}
+                  </ListItem.Subtitle>
+                </ListItem.Content>
+              </ListItem>
+            ))}
+          </ScrollView>
+        ) : (
+          <ListItem style={styles.noItemsContainer} bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title style={styles.noItemTitle}>
+                No more tasks this week!
+              </ListItem.Title>
+              <ListItem.Subtitle style={styles.noItemSubTitle}>
+                Good job!
+              </ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        )}
+      </View>
     </View>
   );
 }
@@ -26,15 +68,54 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    padding: 16,
+    padding: '8%',
+    marginBottom: windowHeight * 0.7,
+  },
+  taskContainer: {
+    width: '100%',
+    marginTop: windowHeight * 0.05,
+  },
+  noItemsContainer: {
+    width: '100%',
+    height: windowHeight * 0.5,
+    marginTop: windowHeight * 0.2,
+  },
+  scrollViewContainer: {
+    width: '100%',
+    height: windowHeight * 0.5,
   },
   title: {
     color: 'white',
     textAlign: 'center',
-    marginBottom: 30,
   },
-  text: {
+  noItemTitle: {
+    color: 'white',
+    alignSelf: 'center',
+    padding: '5%',
+  },
+  noItemSubTitle: {
+    color: 'white',
+    alignSelf: 'center',
+    padding: '5%',
+  },
+  itemTitle: {
+    color: 'white',
+    alignSelf: 'flex-end',
+    padding: '5%',
+  },
+  itemSubTitle: {
+    color: 'white',
+    alignSelf: 'flex-end',
+    padding: '5%',
+  },
+  itemDate: {
+    color: 'white',
+    alignSelf: 'flex-end',
+    padding: '5%',
+  },
+  date: {
     color: 'white',
     textAlign: 'center',
+    marginTop: windowHeight * 0.05,
   },
 });
